14af04a64749e07b3ec5a8015d83b2988bf44b03
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-03-15 23:48:49 -0400

    Add files via upload

---
+++ E5500PCI.sh
@@ -0,0 +1,107 @@
+00:00.0 Host bridge: Intel Corporation Mobile 4 Series Chipset Memory Controller Hub (rev 07)
+	Subsystem: Dell Mobile 4 Series Chipset Memory Controller Hub
+	Kernel modules: intel_agp
+
+00:02.0 VGA compatible controller: Intel Corporation Mobile 4 Series Chipset Integrated Graphics Controller (rev 07)
+	Subsystem: Dell Mobile 4 Series Chipset Integrated Graphics Controller
+	Kernel driver in use: i915
+	Kernel modules: i915
+
+00:02.1 Display controller: Intel Corporation Mobile 4 Series Chipset Integrated Graphics Controller (rev 07)
+	Subsystem: Dell Mobile 4 Series Chipset Integrated Graphics Controller
+
+00:1a.0 USB controller: Intel Corporation 82801I (ICH9 Family) USB UHCI Controller #4 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB UHCI Controller
+	Kernel driver in use: uhci_hcd
+	Kernel modules: uhci_hcd
+
+00:1a.1 USB controller: Intel Corporation 82801I (ICH9 Family) USB UHCI Controller #5 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB UHCI Controller
+	Kernel driver in use: uhci_hcd
+	Kernel modules: uhci_hcd
+
+00:1a.2 USB controller: Intel Corporation 82801I (ICH9 Family) USB UHCI Controller #6 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB UHCI Controller
+	Kernel driver in use: uhci_hcd
+	Kernel modules: uhci_hcd
+
+00:1a.7 USB controller: Intel Corporation 82801I (ICH9 Family) USB2 EHCI Controller #2 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB2 EHCI Controller
+	Kernel driver in use: ehci-pci
+	Kernel modules: ehci_pci
+
+00:1b.0 Audio device: Intel Corporation 82801I (ICH9 Family) HD Audio Controller (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) HD Audio Controller
+	Kernel driver in use: snd_hda_intel
+	Kernel modules: snd_hda_intel
+
+00:1c.0 PCI bridge: Intel Corporation 82801I (ICH9 Family) PCI Express Port 1 (rev 02)
+	Kernel driver in use: pcieport
+
+00:1c.1 PCI bridge: Intel Corporation 82801I (ICH9 Family) PCI Express Port 2 (rev 02)
+	Kernel driver in use: pcieport
+
+00:1c.4 PCI bridge: Intel Corporation 82801I (ICH9 Family) PCI Express Port 5 (rev 02)
+	Kernel driver in use: pcieport
+
+00:1d.0 USB controller: Intel Corporation 82801I (ICH9 Family) USB UHCI Controller #1 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB UHCI Controller
+	Kernel driver in use: uhci_hcd
+	Kernel modules: uhci_hcd
+
+00:1d.1 USB controller: Intel Corporation 82801I (ICH9 Family) USB UHCI Controller #2 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB UHCI Controller
+	Kernel driver in use: uhci_hcd
+	Kernel modules: uhci_hcd
+
+00:1d.2 USB controller: Intel Corporation 82801I (ICH9 Family) USB UHCI Controller #3 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB UHCI Controller
+	Kernel driver in use: uhci_hcd
+	Kernel modules: uhci_hcd
+
+00:1d.7 USB controller: Intel Corporation 82801I (ICH9 Family) USB2 EHCI Controller #1 (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) USB2 EHCI Controller
+	Kernel driver in use: ehci-pci
+	Kernel modules: ehci_pci
+
+00:1e.0 PCI bridge: Intel Corporation 82801 Mobile PCI Bridge (rev 92)
+
+00:1f.0 ISA bridge: Intel Corporation ICH9M LPC Interface Controller (rev 02)
+	Subsystem: Dell ICH9M LPC Interface Controller
+	Kernel driver in use: lpc_ich
+	Kernel modules: lpc_ich
+
+00:1f.2 SATA controller: Intel Corporation 82801IBM/IEM (ICH9M/ICH9M-E) 4 port SATA Controller [AHCI mode] (rev 02)
+	Subsystem: Dell 82801IBM/IEM (ICH9M/ICH9M-E) 4 port SATA Controller [AHCI mode]
+	Kernel driver in use: ahci
+	Kernel modules: ahci
+
+00:1f.3 SMBus: Intel Corporation 82801I (ICH9 Family) SMBus Controller (rev 02)
+	Subsystem: Dell 82801I (ICH9 Family) SMBus Controller
+	Kernel driver in use: i801_smbus
+	Kernel modules: i2c_i801
+
+02:01.0 CardBus bridge: Ricoh Co Ltd RL5c476 II (rev ba)
+	Subsystem: Dell RL5c476 II
+	Kernel driver in use: yenta_cardbus
+	Kernel modules: yenta_socket
+
+02:01.1 FireWire (IEEE 1394): Ricoh Co Ltd R5C832 IEEE 1394 Controller (rev 04)
+	Subsystem: Dell R5C832 IEEE 1394 Controller
+	Kernel driver in use: firewire_ohci
+	Kernel modules: firewire_ohci
+
+02:01.2 SD Host controller: Ricoh Co Ltd R5C822 SD/SDIO/MMC/MS/MSPro Host Adapter (rev 21)
+	Subsystem: Dell R5C822 SD/SDIO/MMC/MS/MSPro Host Adapter
+	Kernel driver in use: sdhci-pci
+	Kernel modules: sdhci_pci
+
+09:00.0 Ethernet controller: Broadcom Inc. and subsidiaries NetXtreme BCM5761e Gigabit Ethernet PCIe (rev 10)
+	Subsystem: Dell NetXtreme BCM5761e Gigabit Ethernet PCIe
+	Kernel driver in use: tg3
+	Kernel modules: tg3
+
+0c:00.0 Network controller: Broadcom Inc. and subsidiaries BCM4312 802.11b/g LP-PHY (rev 01)
+	Subsystem: Dell Wireless 1397 WLAN Mini-Card
+	Kernel driver in use: wl
+	Kernel modules: ssb, wl
